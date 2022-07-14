ea6d9a6969dacc17d6f146719cd28d439c51562b
x <karolina_bp@hotmail.com>
2022-07-14 10:24:26 -0300

    Passando caminho absoluto para conexão com bd sqlite

--- conexao.php
+++ conexao.php
@@ -3,6 +3,13 @@
 
 
 //conexao com banco sqlite
+// parametro 1 = string de conexao com driver do sqlite
+//obs: a recomendacao seja do caminho absoluto
+$caminhoBanco = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:banco.sqlite');
+//PDO = PHP Data Objects - objetos de dados em php
+//interface leve e consistente para acessar um banco de  dados
+//utiliza drivers específicos para cada bd
+// se o banco sqlite nao existir, ele cria, se for um mysql, por exemplo, ele joga um erro na tela
 
 echo 'Conectei';
