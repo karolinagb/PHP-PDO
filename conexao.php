6a8469df74768bbd8102dc132e5aac29ba832bf2
x <karolina_bp@hotmail.com>
2022-07-15 14:27:21 -0300

    INSERT INTO com exec

--- conexao.php
+++ conexao.php
@@ -10,6 +10,9 @@ $pdo = new PDO('sqlite:banco.sqlite');
 //PDO = PHP Data Objects - objetos de dados em php
 //interface leve e consistente para acessar um banco de  dados
 //utiliza drivers específicos para cada bd
-// se o banco sqlite nao existir, ele cria, se for um mysql, por exemplo, ele joga um erro na tela
+// se o banco sqlite nao existir, ele cria, se for um mysql, por exemplo, ele joga um erro na tela 
 
 echo 'Conectei';
+
+//EXECUTAR código sql
+$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
