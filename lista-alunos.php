606fee28aa2668cefd4202ae733280a727b3ad82
x <karolina_bp@hotmail.com>
2022-07-18 12:05:25 -0300

    Buscando uma única coluna com o fetchColumn

--- lista-alunos.php
+++ lista-alunos.php
@@ -7,39 +7,28 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$statement = $pdo->query('SELECT * FROM students');
+$statement = $pdo->query('SELECT * FROM students;');
 
-//Enquanto essa varia studentData que esta recebendo um dado atual do bd tiver um valor válido, um codigo sera executado
-while($studentData = $statement->fetch(PDO::FETCH_ASSOC)){
-    $student = new Student($studentData['id'], $studentData['name'], new DateTimeImmutable($studentData['birth_date']));
-
-    echo $student->age() . PHP_EOL;
-}
-exit();
+//fetchColumn = retorna 1 linha com os dados de 1 única coluna
+var_dump($statement->fetchColumn(1)); exit(); //percorre uma linha de cada vez
 
 //FETCH_ASSOC = TRAZ o indice do array com o nome da coluna
 //PDO::FETCH_CLASS, Student::class vai instanciar a classe student e preencher suas propriedades baseado no nome das colunas do bd
     //O indicado é buscar como array associativo para não ter problema de divergência de nome de coluna com nome de propriedade
-
-    //o metodo fetch busca um objeto só
-    //Posso tambem usar o fetch no lugar do fetchAll quando tenho muitos dados no banco e nao quero colocar eles na memoria ao mesmo tempo
-$studentDataList = $statement->fetch(PDO::FETCH_ASSOC);
+$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
 $studentList = [];
-
-var_dump($studentDataList); exit();
-
-
-foreach($studentDataList as $studentData){
-    $studentList[] = new Student($studentData['id'], $studentData['name'], new DateTimeImmutable($studentData['birth_date']));
-}
 
 var_dump($studentList);
 
+foreach($studentDataList as $studentData){
+    $studentList[] = new Student($studentData['id'], $studentData['name'], new DateTimeImmutable($studentData['birthDate']));
+}
+
 //Pegando o primeiro aluno e exibindo o id
-// echo $studentList[0]['id'] . PHP_EOL;
+echo $studentList[0]['id'] . PHP_EOL;
 
 //ou do primeiro aluno posso exibir a primeira coluna que é o id
-// echo $studentList[0][0];
+echo $studentList[0][0];
 
 //O padrão do fetchAll é trazer os dados representados dessas 2 formas
 //Só que podemos informar como ele deve trazer essa informação
